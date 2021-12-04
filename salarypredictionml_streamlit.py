960e23abaa1366a6c93bb7fcedb384ce71a3e895
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2021-12-04 15:44:50 -0800

    Update salarypredictionml_streamlit.py

--- salarypredictionml_streamlit.py
+++ salarypredictionml_streamlit.py
@@ -4,12 +4,19 @@ from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
+data = pd.read_csv("data//Salary_Data.csv", sep = ",")
+x = np.array(data['YearsExperience']).reshape(-1, 1)
+lr = LinearRegression()
+lr.fit(x, np.array(data['Salary']))
+
 st.title("Salary Predictor")
+st.image("data//SalPred.jpeg", width = 800)
 nav = st.sidebar.radio("Navigation", ["Home", "Prediction", "Contribute"])
 
 if nav == "Home":
-    pass
-
+    if st.checkbox("Show Table"):
+        st.table(data)
+        
 if nav == "Prediction":
     pass
 
